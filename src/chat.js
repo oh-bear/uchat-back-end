065e43b6de3651245131868b617fcb5d30b60309
Airing <gx-deng@163.com>
2017-11-20 08:45:05 +0800

    refactor: 增加开发环境配置

--- src/chat.js
+++ src/chat.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const {KEY, checkToken, md5Pwd} = require('./util')
+const {checkToken} = require('./util')
 
 const model = require('./model')
 const User = model.getModel('user')
@@ -9,40 +9,45 @@ const Chat = model.getModel('chat')
 
 const _filter = {'password': 0, '__v': 0}
 
+const env = process.env.NODE_ENV || 'production';
+
 router.get('/list/:id', function (req, res) {
   const {id} = req.params
   const {token, uid, timestamp} = req.query
-  if (checkToken(uid, timestamp, token)) {
-    Chat.find({'$or': [{from: id}, {to: id}]}, function (err, doc) {
-      if (!err) {
+  if (!checkToken(uid, timestamp, token))
+    return res.json({code: 500, msg: '密钥不正确'})
+  Chat.find({'$or': [{from: id}, {to: id}]}, function (err, doc) {
+    if (!err) {
+      if (env === 'production') {
         const user_ids = doc.map((item, index) => {
           return item.to
         })
-        // User.find({_id: user_ids}, function (err, users) {
-        //   if (!err) return res.json({code: 0, data: users})
-        // })
-        // 开发环境特有，测试需要
-        User.find({}, function (err, users) {
-          if (!err) return res.json({code: 0, data: users})
+        User.find({_id: user_ids}, _filter, function (err, users) {
+          if (err) return res.json({code: 505, msg: '后端出错了'})
+          return res.json({code: 0, data: users})
+        })
+      } else {
+        User.find({}, _filter, function (err, users) {
+          if (err) return res.json({code: 505, msg: '后端出错了'})
+          return res.json({code: 0, data: users})
         })
       }
-    })
-  } else {
-    return res.json({code: 500, msg: '密钥不正确'})
-  }
+    } else {
+      return res.json({code: 505, msg: '后端出错了'})
+    }
+  })
 })
 
 router.get('/record', function (req, res) {
   const {token, uid, timestamp, from, to} = req.query
-  if (checkToken(uid, timestamp, token)) {
-    Chat.find({chat_id: [from, to].sort().join('_')})
-      .sort('-createdAt')
-      .exec(function (err, doc) {
-        if (!err) return res.json({code: 0, data: doc})
-      })
-  } else {
+  if (!checkToken(uid, timestamp, token))
     return res.json({code: 500, msg: '密钥不正确'})
-  }
+  Chat.find({chat_id: [from, to].sort().join('_')})
+    .sort('-createdAt')
+    .exec(function (err, doc) {
+      if (err) return res.json({code: 505, msg: '后端出错了'})
+      return res.json({code: 0, data: doc})
+    })
 })
 
 router.get('/remove', function (req, res) {
